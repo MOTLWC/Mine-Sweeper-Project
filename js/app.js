89f91d0e160258c38d3d709daf081a5aa9cb0958
FelixMR <felixmcneillrutter@gmail.com>
2024-06-23 12:22:54 +0100

    Fixed typo 'documnet' on line 6

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const gridParent = document.getElementById("cellGrid");
 const rowInputElement = document.getElementById("rowCount");
 const columnInputElement = document.getElementById("columnCount");
 const mineInputElement = document.getElementById("mineCount");
-const flagCounterElement = documnet.getElementById("flagDisplay");
+const flagCounterElement = document.getElementById("flagDisplay");
 // ? "cellElementArray" will contain the children elements that will be added to "gridParent" when the game is initialised 
 const cellElementArray = [];
 //!---------------Constants---------------------
