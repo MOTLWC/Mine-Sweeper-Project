2f1e1843410014955674d366ad0a563f8954ccd7
FelixMR <felixmcneillrutter@gmail.com>
2024-06-23 12:19:59 +0100

    Added cached elements

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 //!---------------Cached Elements---------------
+const gridParent = document.getElementById("cellGrid");
+const rowInputElement = document.getElementById("rowCount");
+const columnInputElement = document.getElementById("columnCount");
+const mineInputElement = document.getElementById("mineCount");
+const flagCounterElement = documnet.getElementById("flagDisplay");
+// ? "cellElementArray" will contain the children elements that will be added to "gridParent" when the game is initialised 
+const cellElementArray = [];
 //!---------------Constants---------------------
 //!---------------Variables---------------------
 //!---------------Event Listeners---------------
