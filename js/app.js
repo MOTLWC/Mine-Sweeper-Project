515cba8cd677016db3f4a679031df0338ab5fe30
FelixMR <felixmcneillrutter@gmail.com>
2024-06-23 12:25:27 +0100

    Removed Testing Console.dir

--- js/app.js
+++ js/app.js
@@ -7,11 +7,7 @@ const flagCounterElement = document.getElementById("flagDisplay");
 // ? "cellElementArray" will contain the children elements that will be added to "gridParent" when the game is initialised 
 const cellElementArray = [];
 //!---------------Constants---------------------
+
 //!---------------Variables---------------------
 //!---------------Event Listeners---------------
 //!---------------Functions---------------------
-console.dir(gridParent);
-console.dir(rowInputElement);
-console.dir(columnInputElement);
-console.dir(mineInputElement);
-console.dir(flagCounterElement);
