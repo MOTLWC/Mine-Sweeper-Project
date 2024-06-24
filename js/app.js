bcef5b60420ac956388622c33e5d3be70f5496b4
FelixMR <felixmcneillrutter@gmail.com>
2024-06-24 10:44:06 +0100

    Removing console.logs to clean the terminal

--- js/app.js
+++ js/app.js
@@ -22,7 +22,6 @@ const deleteChildren = () => {
     while (gridParent.firstChild) {
         gridParent.removeChild(gridParent.firstChild);
         cellElementArray.shift();
-        console.log(cellElementArray);
     }
 }
 const resetVars = () => {
